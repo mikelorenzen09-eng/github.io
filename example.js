38e861e9e4ec84e32acd87db8fa237148f16c21e
mike <mikelorenzen09@gmail.com>
2026-02-14 23:54:29 +0100

    example

--- example.js
+++ example.js
@@ -1,1 +1,9 @@
-document.querySelector("h1").textContent = "new content"
+const header = document.querySelector('.site-header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.style.boxShadow = '0 12px 35px rgba(0,0,0,0.24)';
+  } else {
+    header.style.boxShadow = '0 10px 30px rgba(0,0,0,0.12)';
+  }
+});
